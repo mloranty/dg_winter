172dd73729d603b3e86e61bc712266bf11397a48
brianweber338 <brianweber338@gmail.com>
2022-03-01 13:39:27 -0500

    Created dg_winter_code Rproj

---
+++ winter_data_code.R
@@ -0,0 +1,18 @@
+
+# Read in Air Temperature data
+dgAirTemp <- read.csv("C:\\Users\\brian\\OneDrive\\Documents\\GEOG331\\Project Data\\data\\dg_air_temperature.csv",
+                      stringsAsFactors = T)
+# Read in Relative Humidity data
+dgRelativeHumid <- read.csv("C:\\Users\\brian\\OneDrive\\Documents\\GEOG331\\Project Data\\data\\dg_relative_humidity.csv",
+                      stringsAsFactors = T)
+# Read in PAR data
+dgPAR <- read.csv("C:\\Users\\brian\\OneDrive\\Documents\\GEOG331\\Project Data\\data\\dg_par.csv",
+                            stringsAsFactors = T)
+# Read in Soil Temperature data
+dgSoilTemp <- read.csv("C:\\Users\\brian\\OneDrive\\Documents\\GEOG331\\Project Data\\data\\dg_soil_temperature.csv",
+                  stringsAsFactors = T)
+# Read in Soil Moisture data
+dgSoilMoist <- read.csv("C:\\Users\\brian\\OneDrive\\Documents\\GEOG331\\Project Data\\data\\dg_soil_moisture.csv",
+                  stringsAsFactors = T)
+
+
