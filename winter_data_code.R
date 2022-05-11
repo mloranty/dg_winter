59805688a15405393fd5d5dea11aa3da3d2d4192
brianweber338 <brianweber338@gmail.com>
2022-05-11 18:50:39 -0400

    Quality of Life improvements

--- winter_data_code.R
+++ winter_data_code.R
@@ -280,7 +280,7 @@ TukeyHSD(SoilTemp.aov)
 winterDailyTempAvgModify$density <- as.character(winterDailyTempAvgModify$density)
 winterDailyTempAvgModify$density <- factor(winterDailyTempAvgModify$density,levels=c("HIGH","MED","LOW"))
 
-# Create box plot for PAR, exclude par values below 10 due to shorter days in the winter
+# Create box plot for soil temperature
 ggplot(data = winterDailyTempAvgModify, aes(x=density,y=t_soil.x,fill=density)) +
   geom_boxplot() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"))+
@@ -289,6 +289,14 @@ ggplot(data = winterDailyTempAvgModify, aes(x=density,y=t_soil.x,fill=density)) 
   theme(legend.position = "none")
 
 
+
+#### Table Data Work ####
+
+DensitySoilTemp <- aggregate(t_soil.x ~ density, data = winterDailyTempAvgModify, FUN = mean, na.rm = TRUE)
+DensityPAR <- aggregate(par.x ~ density, data = winterDailyPARAvg, FUN = mean, na.rm = TRUE)
+
+NFactorsToAverage <- unique(winterDailyTempAvgModify[, c("FreezingNFactor", "density")])
+DensityNFactor <- aggregate(FreezingNFactor ~ density, data = NFactorsToAverage, FUN = mean, na.rm = TRUE)
 
 #### Create N Factor Graph ####
 
@@ -302,26 +310,26 @@ nFactors$FreezingNFactor = as.numeric(nFactors$FreezingNFactor)
 row.names(nFactors) <- 1:nrow(nFactors) 
 
 
-nFactors[c(18,21),"FreezingNFactor"] <- NA
+nFactors[c(18,21, 22),"FreezingNFactor"] <- NA
 
 siteColors = c("DAVY" = "darkgreen", "HDF1" = "chartreuse3",
                "MDF1" = "indianred1", "MDF2" = "indianred3",
                "LBR" = "blue", "LDF2" = "cyan4")
 
 
-plot(x = nFactors[nFactors$site == "HDF1",]$wyear, y = nFactors[nFactors$site == "HDF1",]$FreezingNFactor,type = "b",col = "chartreuse3",
+plot(x = nFactors[nFactors$site == "HDF1",]$wyear, y = nFactors[nFactors$site == "HDF1",]$FreezingNFactor,type = "b",col = "indianred1",
      xlab = "Water Year (October 1st to April 30th)", ylab = "N Factor (Out of 1)", main = "Freezing Degree Days N Factor by Water Year from 2015 to 2019", ylim=c(0.0,0.45), lwd = 3)
 grid(nx = NA, ny = NULL, lty = 2, lwd = 1, col = "black")
 
-lines(x = nFactors[nFactors$site == "DAVY",]$wyear, y = nFactors[nFactors$site == "DAVY",]$FreezingNFactor, type = "b", col = "darkgreen", lwd = 3)
-lines(x = nFactors[nFactors$site == "MDF2",]$wyear, y = nFactors[nFactors$site == "MDF2",]$FreezingNFactor, type = "b", col = "indianred4", lwd = 3)
+lines(x = nFactors[nFactors$site == "DAVY",]$wyear, y = nFactors[nFactors$site == "DAVY",]$FreezingNFactor, type = "b", col = "indianred4", lwd = 3)
+lines(x = nFactors[nFactors$site == "MDF2",]$wyear, y = nFactors[nFactors$site == "MDF2",]$FreezingNFactor, type = "b", col = "darkgreen", lwd = 3)
 lines(x = nFactors[nFactors$site == "LBR",]$wyear, y = nFactors[nFactors$site == "LBR",]$FreezingNFactor, type = "b", col = "blue", lwd = 3)
-lines(x = nFactors[nFactors$site == "MDF1",]$wyear, y = nFactors[nFactors$site == "MDF1",]$FreezingNFactor, type = "b", col = "indianred1", lwd = 3)
+lines(x = nFactors[nFactors$site == "MDF1",]$wyear, y = nFactors[nFactors$site == "MDF1",]$FreezingNFactor, type = "b", col = "chartreuse3", lwd = 3)
 lines(x = nFactors[nFactors$site == "LDF2",]$wyear, y = nFactors[nFactors$site == "LDF2",]$FreezingNFactor, type = "b", col = "cyan4", lwd = 3)
 
 legend("topright", c("DAVY", "HDF1", "MDF1", "MDF2", "LBR", "LDF2"), 
        lty = c(1,1), lwd = 3, cex=0.6, 
-       col = c("darkgreen", "chartreuse3", "indianred1", "indianred4", "blue", "cyan4"))
+       col = c("indianred4", "indianred1", "chartreuse3", "darkgreen", "blue", "cyan4"))
 
 
 #### Create Avg PAR by water year graph ####
@@ -336,18 +344,18 @@ row.names(PARToGraph) <- 1:nrow(PARToGraph)
 
 PARToGraph[c(18,21,22),"PARWaterYearAvg"] <- NA
 
-plot(x = PARToGraph[PARToGraph$site == "HDF1",]$wyear, y = PARToGraph[PARToGraph$site == "HDF1",]$PARWaterYearAvg,type = "b",col = "chartreuse3",
+plot(x = PARToGraph[PARToGraph$site == "HDF1",]$wyear, y = PARToGraph[PARToGraph$site == "HDF1",]$PARWaterYearAvg,type = "b",col = "indianred1",
      xlab = "Water Year (October 1st to April 30th)", ylab = "PAR (µmol/(m²·s))", main = "Average PAR by Water Year from 2015 to 2019", ylim=c(0,80), lwd = 3) 
 
-lines(x = PARToGraph[PARToGraph$site == "DAVY",]$wyear, y = PARToGraph[PARToGraph$site == "DAVY",]$PARWaterYearAvg, type = "b", col = "darkgreen", lwd = 3)
-lines(x = PARToGraph[PARToGraph$site == "MDF1",]$wyear, y = PARToGraph[PARToGraph$site == "MDF1",]$PARWaterYearAvg, type = "b", col = "indianred1", lwd = 3)
-lines(x = PARToGraph[PARToGraph$site == "MDF2",]$wyear, y = PARToGraph[PARToGraph$site == "MDF2",]$PARWaterYearAvg, type = "b", col = "indianred4", lwd = 3)
+lines(x = PARToGraph[PARToGraph$site == "DAVY",]$wyear, y = PARToGraph[PARToGraph$site == "DAVY",]$PARWaterYearAvg, type = "b", col = "indianred4", lwd = 3)
+lines(x = PARToGraph[PARToGraph$site == "MDF1",]$wyear, y = PARToGraph[PARToGraph$site == "MDF1",]$PARWaterYearAvg, type = "b", col = "chartreuse3", lwd = 3)
+lines(x = PARToGraph[PARToGraph$site == "MDF2",]$wyear, y = PARToGraph[PARToGraph$site == "MDF2",]$PARWaterYearAvg, type = "b", col = "darkgreen", lwd = 3)
 lines(x = PARToGraph[PARToGraph$site == "LBR",]$wyear, y = PARToGraph[PARToGraph$site == "LBR",]$PARWaterYearAvg, type = "b", col = "blue", lwd = 3)
 lines(x = PARToGraph[PARToGraph$site == "LDF2",]$wyear, y = PARToGraph[PARToGraph$site == "LDF2",]$PARWaterYearAvg, type = "b", col = "cyan4", lwd = 3)
 
 legend("topright", c("DAVY", "HDF1", "MDF1", "MDF2", "LBR", "LDF2"), 
        lty = c(1,1), lwd = 3, cex=0.8, 
-       col = c("darkgreen", "chartreuse3", "indianred1", "indianred4", "blue", "cyan4"))
+       col = c("indianred4", "indianred1", "chartreuse3", "darkgreen", "blue", "cyan4"))
 
 
 #### Create DAVY Graphs ####
@@ -431,17 +439,18 @@ DAVYWinterPARAvg <- filter(DAVYWinterPARAvg, wdoy != 213)
 
 
 
-plot(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2016",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2016",]$PARDailyAvg,type = "b",col = "darkgreen",
+plot(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2016",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2016",]$PARDailyAvg,type = "b",col = "chartreuse3",
      xlab = "Water Year by DOY (October 1st to April 30th)", ylab = "PAR (µmol/(m²·s))", main = "Average Daily PAR at DAVY Site Over the Water Year", lwd = 2, ylim=c(0,475)) 
 
-lines(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2017",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2017",]$PARDailyAvg, type = "b", col = "indianred4", lwd = 2)
-lines(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2018",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2018",]$PARDailyAvg, type = "b", col = "indianred1", lwd = 2)
+lines(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2017",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2017",]$PARDailyAvg, type = "b", col = "indianred1", lwd = 2)
+lines(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2018",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2018",]$PARDailyAvg, type = "b", col = "indianred4", lwd = 2)
 lines(x = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2019",]$wdoy, y = DAVYWinterPARAvg[DAVYWinterPARAvg$wyear == "2019",]$PARDailyAvg, type = "b", col = "blue", lwd = 2)
 
 
 legend("topleft", c("2016", "2017", "2018", "2019"), 
        lty = c(1,1), lwd = 3, cex=0.8, 
-       col = c("darkgreen", "indianred4", "indianred1", "blue"))
+       col = c("chartreuse3", "indianred1", "indianred4", "blue"))
+
 
 
 
