3f29674bfd0fb953fb31f4aba5aa8887195b70a2
Mike Loranty <mloranty@colgate.edu>
2023-11-29 13:14:57 -0500

    Analyzing data for AGU. Classic!

--- dg_met_analyses.R
+++ dg_met_analyses.R
@@ -19,7 +19,7 @@ rm(list = ls())
 wy <- function(x)
 {
   #ifelse(is.POSIXct(x),,"Input is not Date/Time class")
-  ifelse(month(x)<10,year(x),year(x)-1)
+  ifelse(month(x)<10,year(x),year(x)+1)
 }
 
 # calculate water day from timestamp
@@ -70,25 +70,83 @@ nr <- read.csv("https://cn.dataone.org/cn/v2/resolve/urn:uuid:229ad93b-ddfe-4e3f
 # viper soil temp
 tsv <- read.csv("https://cn.dataone.org/cn/v2/resolve/urn:uuid:753917ef-7505-42c0-baa4-a1863cc1077d", sep = ",", header = T)
 
-# last read snowdepth data from Cherskiy met station(s)
+# last read snowdepth data from Cherskiy met station(s) 
 snw <- read.csv("L:/data_repo/field_data/siberia_climate_data/cherskiy_met_1940_2021.csv", sep=",",header = T)
 
+# add timestamp
 snw$timestamp = as.POSIXct(snw$DATE, format = "%Y-%m-%d")
 
-snw <- snw[which(year(snw$timestamp) > 2010),]
+# add water year
+snw$wy <- wy(snw$timestamp)
+
+# fix erroneous high values (mostly typos - determined by comparing to surrounding days)
+snw$SNWD[which(snw$DATE == "2006-12-09")] <- 311
+snw$SNWD[which(snw$DATE == "2009-11-26")] <- 220
+snw$SNWD[which(snw$DATE == "2019-09-30")] <- 31
+snw$SNWD[which(snw$DATE == "2021-09-25")] <- NA
+
+#subset to incude only data since 2000
+snw2 <- snw[which(year(snw$timestamp) > 2000),]
+
 # aggregate soil and air temperature to daily values
 tsd <- ts %>%
   group_by(year, doy, site, sensorZ) %>%
   summarise(t_soil = mean(t_soil))
 
+# add timestamp
 tsd$timestamp = as.POSIXct(paste(tsd$year,tsd$doy,sep="-"), format = "%Y-%j")
+
+#add water year
+tsd$wy <- wy(tsd$timestamp)
+
+#calculate fdd
+tsa <- tsd %>%
+  group_by(wy,site,sensorZ) %>%
+  summarise(ts.fdd = fdd(t_soil),
+            ts.mean = mean(t_soil),
+            ts.obs = length(t_soil ==T))
 
 # aggregate air temperature to daily
 tad <- ta %>%
   group_by(year, doy, site) %>%
   summarise(t_air = mean(t_air))
 
+# add timestamp
 tad$timestamp = as.POSIXct(paste(tad$year,tad$doy,sep="-"), format = "%Y-%j")
+
+#add water year
+tad$wy <- wy(tad$timestamp)
+
+#calculate fdd
+taa <- tad %>%
+  group_by(wy,site) %>%
+  summarise(ta.fdd = fdd(t_air),
+            ta.mean = mean(t_air),
+            ta.obs = length(t_air ==T))
+
+# summarise annual snow depth by water year
+snwy <- snw2 %>%
+  group_by(wy) %>%
+  summarise(t5 = quantile(SNWD,na.rm=T, prob = 0.95), 
+            max = max(SNWD, na.rm = T), 
+            mean = mean(SNWD, na.rm = T),
+            med = median(SNWD, na.rm = T))
+
+# join the  temp summaries
+tann <- full_join(tsa,taa)
+
+#calculate freezing n-factor
+tann$nf <- tann$ts.fdd/tann$ta.fdd
+
+#join snow depth data
+tann <- full_join(tann,snwy)
+
+tann.om <- filter(tann, site == "DAVY" & sensorZ == -10 | 
+                        site == "HDF1" & sensorZ ==-9 |
+                        site =="LBR" & sensorZ == -18 |
+                        site =="LDF2" & sensorZ == -8 |
+                        site == "MDF1" & sensorZ == -6|
+                        site == "MDF2" & sensorZ == -9)
 
 # make a few plots to summarize the existing data (and associated gaps)
 #####
@@ -107,10 +165,10 @@ ggplot(tsd, aes(x = timestamp, y = t_soil, group = site, color = site)) +
   scale_x_datetime(labels = label_date("%Y-%m-%d"))
 
 # timeseries of snow depth data
-ggplot(snw, aes(x = timestamp, y = SNWD)) +
+ggplot(snw2, aes(x = timestamp, y = SNWD)) +
   geom_line() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  scale_x_datetime(labels = label_date("%Y-%m-%d"))
+  scale_x_datetime(labels = label_date("%Y"))
 # plots of cumulative freezing degree days each winter? 
 # plots of cumulative heat flux each winter? 
 
